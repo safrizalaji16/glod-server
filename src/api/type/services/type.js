67b8bd7c9b9d0b5a2362d8a03dfab01aee5f0cff
safrizal <safrizalaji16@gmail.com>
2024-02-26 15:25:41 +0700

    feat: dummy data

--- src/api/type/services/type.js
+++ src/api/type/services/type.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * image service
+ * type service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::image.image');
+module.exports = createCoreService('api::type.type');
