83427dd5391bb7c1868a01090050e1eaee19d63e
safrizal <safrizalaji16@gmail.com>
2024-02-28 11:52:13 +0700

    socket

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -16,5 +16,34 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    let { Server } = require("socket.io");
+    let io = new Server(strapi.server.httpServer, {
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+      },
+    });
+
+    io.on("connection", (socket) => {
+      console.log("User connected", socket.id);
+
+      // Setelah 3 detik, kirimkan link video kepada klien
+      setTimeout(() => {
+        socket.emit("linkVideo", "ini link nya");
+      }, 3000);
+
+      // Setelah 5 detik, lakukan auto disconnect
+      setTimeout(() => {
+        socket.disconnect(true); // Parameter true berarti disconnect secara paksa
+        console.log("Auto disconnecting user", socket.id);
+      }, 5000);
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected", socket.id);
+      });
+    });
+
+    strapi.io = io;
+  },
 };
