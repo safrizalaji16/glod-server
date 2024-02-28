4a4ddeb78ad169d4e7fd6221ac5b63ffe98e112d
safrizal <safrizalaji16@gmail.com>
2024-02-28 17:02:28 +0700

    refactor database

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,24 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface ProductTypes extends Schema.Component {
+  collectionName: 'components_product_types_types';
+  info: {
+    displayName: 'type';
+    icon: 'apps';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required & Attribute.Unique;
+    images: Attribute.Media & Attribute.Required;
+    status: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<false>;
+    sizes: Attribute.Relation<'product.types', 'oneToMany', 'api::size.size'>;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'product.types': ProductTypes;
+    }
+  }
 }
